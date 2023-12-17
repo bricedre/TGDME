22d27d6d8bc6c2209795c4d664415b6c4604c86e
SypherLudik <idscrafter@gmail.com>
2023-12-17 07:04:26 +0100

    grosse fonction de render

--- src/components/assetLoader.js
+++ src/components/assetLoader.js
@@ -1,24 +1,24 @@
-const {rootPath} = require("electron-root-path");
-const fs = require('fs');
+const { rootPath } = require("electron-root-path");
+const fs = require("fs");
 
 import { currentCollectionIndex } from "./collectionManager.js";
 
 export let assetsLibrary = {};
 export let errorImage;
 
-export function loadAssets(p){
-
+export function loadAssets(p) {
   assetsLibrary = {};
 
-  let assetsPath = rootPath+"/src/decks/"+currentCollectionIndex+"/assets/";
+  let assetsPath =
+    rootPath + "/src/decks/" + currentCollectionIndex + "/assets/";
 
-  var currentAssetsList = getFiles(assetsPath)
-  if(currentAssetsList){
+  var currentAssetsList = getFiles(assetsPath);
+  if (currentAssetsList) {
     currentAssetsList.forEach((asset) => {
       let file = asset.split("//")[1];
       let fileName = file.split(".")[0];
       let extension = file.split(".")[1];
-  
+
       if (extension === "jpg" || extension === "png") {
         let img = p.loadImage(asset);
         assetsLibrary[fileName] = img;
@@ -27,23 +27,23 @@ export function loadAssets(p){
     });
   }
 
-  errorImage = p.loadImage(rootPath+"/assets/imageError.png");
+  errorImage = p.loadImage(rootPath + "/assets/imageError.png");
 }
 
 function getFiles(dir, files = []) {
   // Get an array of all files and directories in the passed directory using fs.readdirSync
-  const fileList = fs.readdirSync(dir)
+  const fileList = fs.readdirSync(dir);
   // Create the full path of the file/directory by concatenating the passed directory and file/directory name
   for (const file of fileList) {
-    const name = `${dir}/${file}`
+    const name = `${dir}/${file}`;
     // Check if the current file/directory is a directory using fs.statSync
     if (fs.statSync(name).isDirectory()) {
       // If it is a directory, recursively call the getFiles function with the directory path and the files array
-      getFiles(name, files)
+      getFiles(name, files);
     } else {
       // If it is a file, push the full path to the files array
-      files.push(name)
+      files.push(name);
     }
   }
-  return files
+  return files;
 }
--- src/components/elements.js
+++ src/components/elements.js
@@ -4,83 +4,34 @@ import { currentCollection } from "./collectionManager.js";
 
 export function renderImageComponent(p5, componentIndex, elementIndex) {
   let currentElements = currentCollection.elements;
-  let currentCollectionInfo = currentCollection.collectionInfo;
   let currentTemplate = currentCollection.template;
 
   let cardData = currentElements[elementIndex];
+  let templateData = currentTemplate[componentIndex];
 
-  let _src,
-    _img,
-    _type,
-    _position_x,
-    _position_y,
-    _width,
-    _height,
-    _anchor,
-    _angle,
-    _tint,
-    _opacity;
+  let _img, _type, _position_x, _position_y, _width, _height, _anchor, _angle, _tint, _opacity;
 
   // No render if no src
 
-  if (
-    currentTemplate[componentIndex].trigger.value == "" ||
-    cardData[currentTemplate[componentIndex].trigger.value]
-  ) {
-    _src = getActualValue(currentTemplate[componentIndex].src, "", false);
+  if (templateData.trigger.value == "" || cardData[templateData.trigger.value]) {
+    var _src = getActualValue(templateData.src, "", false);
 
-    _type = currentTemplate[componentIndex].src.type;
+    _type = templateData.src.type;
 
     if (_type === "0") {
-      _img = assetsLibrary[currentTemplate[componentIndex].src.value];
+      _img = assetsLibrary[_src.value];
     } else if (_type === "1") {
-      _img =
-        assetsLibrary[
-          currentElements[elementIndex][
-            currentTemplate[componentIndex].src.value
-          ]
-        ];
+      _img = assetsLibrary[currentElements[elementIndex][_src.value]];
     }
 
-    _position_x = getActualValue(
-      currentTemplate[componentIndex].position_x,
-      0,
-      true
-    );
-
-    _position_y = getActualValue(
-      currentTemplate[componentIndex].position_y,
-      0,
-      true
-    );
-
-    _width = getActualValue(currentTemplate[componentIndex].width, "W", true);
-
-    _height = getActualValue(currentTemplate[componentIndex].height, "W", true);
-
-    _anchor = getActualValue(
-      currentTemplate[componentIndex].anchor,
-      p5.CENTER,
-      true
-    );
-
-    _angle = getActualValue(
-      currentTemplate[componentIndex].angle,
-      0,
-      true
-    );
-
-    _tint = getActualValue(
-      currentTemplate[componentIndex].tint,
-      "#FFFFFF",
-      false
-    );
-
-    _opacity = getActualValue(
-      currentTemplate[componentIndex].opacity,
-      100,
-      true
-    );
+    _position_x = getActualValue(templateData.position_x, 0, true);
+    _position_y = getActualValue(templateData.position_y, 0, true);
+    _width = getActualValue(templateData.width, "W", true);
+    _height = getActualValue(templateData.height, "W", true);
+    _anchor = getActualValue(templateData.anchor, p5.CENTER, true);
+    _angle = getActualValue(templateData.angle, 0, true);
+    _tint = getActualValue(templateData.tint, "#FFFFFF", false);
+    _opacity = getActualValue(templateData.opacity, 100, true);
 
     p5.card.push();
     try {
@@ -100,14 +51,9 @@ export function renderImageComponent(p5, componentIndex, elementIndex) {
 
 export function renderTextComponent(p5, componentIndex, elementIndex) {
   let currentElements = currentCollection.elements;
-  let currentCollectionInfo = currentCollection.collectionInfo;
   let currentTemplate = currentCollection.template;
 
   let cardData = currentElements[elementIndex];
-
-  var W = currentCollectionInfo.W * currentCollectionInfo.resolution;
-  var H = currentCollectionInfo.H * currentCollectionInfo.resolution;
-  var resolution = currentCollectionInfo.resolution;
 
   let _textToWrite = "";
   let _type = "";
@@ -121,10 +67,7 @@ export function renderTextComponent(p5, componentIndex, elementIndex) {
   let _angle;
 
   // No render if no src
-  if (
-    currentTemplate[componentIndex].trigger.value == "" ||
-    cardData[currentTemplate[componentIndex].trigger.value]
-  ) {
+  if (currentTemplate[componentIndex].trigger.value == "" || cardData[currentTemplate[componentIndex].trigger.value]) {
     if (currentTemplate[componentIndex].src) {
       if (currentTemplate[componentIndex].src.type) {
         _type = currentTemplate[componentIndex].src.type;
@@ -141,13 +84,9 @@ export function renderTextComponent(p5, componentIndex, elementIndex) {
         ? eval(currentTemplate[componentIndex].position_y.value)
         : 0;
 
-      _size = currentTemplate[componentIndex].size
-        ? eval(currentTemplate[componentIndex].size.value)
-        : 20;
+      _size = currentTemplate[componentIndex].size ? eval(currentTemplate[componentIndex].size.value) : 20;
 
-      _color = currentTemplate[componentIndex].color
-        ? currentTemplate[componentIndex].color.value
-        : "#000000";
+      _color = currentTemplate[componentIndex].color ? currentTemplate[componentIndex].color.value : "#000000";
 
       _align = currentTemplate[componentIndex].align
         ? eval("p5." + currentTemplate[componentIndex].align.value)
@@ -158,13 +97,9 @@ export function renderTextComponent(p5, componentIndex, elementIndex) {
           ? assetsLibrary[currentTemplate[componentIndex].font.value]
           : "Verdana";
 
-      _opacity = currentTemplate[componentIndex].opacity
-        ? currentTemplate[componentIndex].opacity.value
-        : "100";
+      _opacity = currentTemplate[componentIndex].opacity ? currentTemplate[componentIndex].opacity.value : "100";
 
-      _angle = currentTemplate[componentIndex].angle
-        ? currentTemplate[componentIndex].angle.value
-        : "0";
+      _angle = currentTemplate[componentIndex].angle ? currentTemplate[componentIndex].angle.value : "0";
 
       p5.card.push();
       try {
@@ -189,14 +124,9 @@ export function renderTextComponent(p5, componentIndex, elementIndex) {
 
 export function renderStripComponent(p5, componentIndex, elementIndex) {
   let currentElements = currentCollection.elements;
-  let currentCollectionInfo = currentCollection.collectionInfo;
   let currentTemplate = currentCollection.template;
 
   let cardData = currentElements[elementIndex];
-
-  var W = currentCollectionInfo.W * currentCollectionInfo.resolution;
-  var H = currentCollectionInfo.H * currentCollectionInfo.resolution;
-  var resolution = currentCollectionInfo.resolution;
 
   let _imgs = [];
   let _position_x;
@@ -213,10 +143,7 @@ export function renderStripComponent(p5, componentIndex, elementIndex) {
   let _totalHeight;
 
   // No render if no src
-  if (
-    currentTemplate[componentIndex].trigger == null ||
-    cardData[template[componentIndex].trigger]
-  ) {
+  if (currentTemplate[componentIndex].trigger == null || cardData[template[componentIndex].trigger]) {
     if (currentTemplate[componentIndex].src) {
       var elementsList = cardData[currentTemplate[componentIndex].src]
         ? cardData[currentTemplate[componentIndex].src]
@@ -228,47 +155,22 @@ export function renderStripComponent(p5, componentIndex, elementIndex) {
         }
 
         _position_x =
-          currentTemplate[componentIndex].position_x != null
-            ? eval(currentTemplate[componentIndex].position_x)
-            : 0;
+          currentTemplate[componentIndex].position_x != null ? eval(currentTemplate[componentIndex].position_x) : 0;
         _position_y =
-          currentTemplate[componentIndex].position_y != null
-            ? eval(currentTemplate[componentIndex].position_y)
-            : 0;
-        _width = currentTemplate[componentIndex].width
-          ? eval(currentTemplate[componentIndex].width)
-          : 100;
-        _height = currentTemplate[componentIndex].height
-          ? eval(currentTemplate[componentIndex].height)
-          : 100;
-        _spacing_x = currentTemplate[componentIndex].spacing_x
-          ? eval(currentTemplate[componentIndex].spacing_x)
-          : 0;
-        _spacing_y = currentTemplate[componentIndex].spacing_y
-          ? eval(currentTemplate[componentIndex].spacing_y)
-          : 0;
-        _style = currentTemplate[componentIndex].style
-          ? currentTemplate[componentIndex].style
-          : "straight";
-        _offset_x = currentTemplate[componentIndex].offset_x
-          ? eval(currentTemplate[componentIndex].offset_x)
-          : 0;
-        _offset_y = currentTemplate[componentIndex].offset_y
-          ? eval(currentTemplate[componentIndex].offset_y)
-          : 0;
-        _align = currentTemplate[componentIndex].align
-          ? currentTemplate[componentIndex].align
-          : "LEFT";
+          currentTemplate[componentIndex].position_y != null ? eval(currentTemplate[componentIndex].position_y) : 0;
+        _width = currentTemplate[componentIndex].width ? eval(currentTemplate[componentIndex].width) : 100;
+        _height = currentTemplate[componentIndex].height ? eval(currentTemplate[componentIndex].height) : 100;
+        _spacing_x = currentTemplate[componentIndex].spacing_x ? eval(currentTemplate[componentIndex].spacing_x) : 0;
+        _spacing_y = currentTemplate[componentIndex].spacing_y ? eval(currentTemplate[componentIndex].spacing_y) : 0;
+        _style = currentTemplate[componentIndex].style ? currentTemplate[componentIndex].style : "straight";
+        _offset_x = currentTemplate[componentIndex].offset_x ? eval(currentTemplate[componentIndex].offset_x) : 0;
+        _offset_y = currentTemplate[componentIndex].offset_y ? eval(currentTemplate[componentIndex].offset_y) : 0;
+        _align = currentTemplate[componentIndex].align ? currentTemplate[componentIndex].align : "LEFT";
 
-        _totalWidth =
-          (elementsList.length * Math.min(_width, _spacing_x) +
-            (elementsList.length - 2) * _spacing_x) /
-          2;
+        _totalWidth = (elementsList.length * Math.min(_width, _spacing_x) + (elementsList.length - 2) * _spacing_x) / 2;
 
         _totalHeight =
-          (elementsList.length * Math.min(_height, _spacing_y) +
-            (elementsList.length - 1) * _spacing_y) /
-          2;
+          (elementsList.length * Math.min(_height, _spacing_y) + (elementsList.length - 1) * _spacing_y) / 2;
 
         try {
           p5.card.imageMode(p5.CENTER);
@@ -279,20 +181,12 @@ export function renderStripComponent(p5, componentIndex, elementIndex) {
                 _position_x +
                   (elementsList.length > 1
                     ? (_spacing_x > 0 ? i * _spacing_x - _totalWidth / 2 : 0) +
-                      (_style == "alternate"
-                        ? i % 2 == 0
-                          ? _offset_x
-                          : -_offset_x
-                        : 0)
+                      (_style == "alternate" ? (i % 2 == 0 ? _offset_x : -_offset_x) : 0)
                     : 0),
                 _position_y +
                   (elementsList.length > 1
                     ? (_spacing_y > 0 ? i * _spacing_y - _totalHeight / 2 : 0) +
-                      (_style == "alternate"
-                        ? i % 2 == 0
-                          ? _offset_y
-                          : -_offset_y
-                        : 0)
+                      (_style == "alternate" ? (i % 2 == 0 ? _offset_y : -_offset_y) : 0)
                     : 0),
                 _width,
                 _height
@@ -302,21 +196,11 @@ export function renderStripComponent(p5, componentIndex, elementIndex) {
                 _imgs[i],
                 _position_x +
                   (elementsList.length > 1
-                    ? i * _spacing_x +
-                      (_style == "alternate"
-                        ? i % 2 == 0
-                          ? _offset_x
-                          : -_offset_x
-                        : 0)
+                    ? i * _spacing_x + (_style == "alternate" ? (i % 2 == 0 ? _offset_x : -_offset_x) : 0)
                     : 0),
                 _position_y +
                   (elementsList.length > 1
-                    ? i * _spacing_y +
-                      (_style == "alternate"
-                        ? i % 2 == 0
-                          ? _offset_y
-                          : -_offset_y
-                        : 0)
+                    ? i * _spacing_y + (_style == "alternate" ? (i % 2 == 0 ? _offset_y : -_offset_y) : 0)
                     : 0),
                 _width,
                 _height
@@ -327,20 +211,12 @@ export function renderStripComponent(p5, componentIndex, elementIndex) {
                 _position_x +
                   (elementsList.length > 1
                     ? (_spacing_x > 0 ? i * _spacing_x - _totalWidth : 0) +
-                      (_style == "alternate"
-                        ? i % 2 == 0
-                          ? _offset_x
-                          : -_offset_x
-                        : 0)
+                      (_style == "alternate" ? (i % 2 == 0 ? _offset_x : -_offset_x) : 0)
                     : 0),
                 _position_y +
                   (elementsList.length > 1
                     ? (_spacing_y > 0 ? i * _spacing_y - _totalHeight : 0) +
-                      (_style == "alternate"
-                        ? i % 2 == 0
-                          ? _offset_y
-                          : -_offset_y
-                        : 0)
+                      (_style == "alternate" ? (i % 2 == 0 ? _offset_y : -_offset_y) : 0)
                     : 0),
                 _width,
                 _height
@@ -352,6 +228,78 @@ export function renderStripComponent(p5, componentIndex, elementIndex) {
         }
       }
     }
+  }
+}
+
+export function renderComponent(p5, componentType, componentIndex, elementIndex) {
+  let currentElements = currentCollection.elements;
+  let currentTemplate = currentCollection.template;
+
+  let cardData = currentElements[elementIndex];
+  let templateData = currentTemplate[componentIndex];
+
+  let _imgs,
+    _img,
+    _textToWrite,
+    _type,
+    _position_x,
+    _position_y,
+    _width,
+    _height,
+    _size,
+    _font,
+    _anchor,
+    _angle,
+    _color,
+    _tint,
+    _opacity,
+    _spacing_x,
+    _spacing_y,
+    _style,
+    _offset_x,
+    _offset_y,
+    _totalWidth,
+    _totalHeight;
+
+  // No render if no src
+
+  if (templateData.trigger.value == "" || cardData[templateData.trigger.value]) {
+    var _src = getActualValue(templateData.src, "", false);
+
+    _type = templateData.src.type;
+
+    if (_type === "0") {
+      if (componentType == "IMAGE") {
+        _img = assetsLibrary[_src.value];
+      }
+    } else if (_type === "1") {
+      if (componentType == "IMAGE") {
+        _img = assetsLibrary[currentElements[elementIndex][_src.value]];
+      }
+    }
+
+    _position_x = getActualValue(templateData.position_x, 0, true);
+    _position_y = getActualValue(templateData.position_y, 0, true);
+    _width = getActualValue(templateData.width, "W", true);
+    _height = getActualValue(templateData.height, "W", true);
+    _anchor = getActualValue(templateData.anchor, p5.CENTER, true);
+    _angle = getActualValue(templateData.angle, 0, true);
+    _tint = getActualValue(templateData.tint, "#FFFFFF", false);
+    _opacity = getActualValue(templateData.opacity, 100, true);
+
+    p5.card.push();
+    try {
+      p5.card.angleMode(p5.DEGREES);
+      p5.card.imageMode(_anchor);
+      p5.card.translate(_position_x, _position_y);
+      if (_angle !== 0) p5.card.rotate(_angle);
+      p5.card.tint(_tint + Math.round(_opacity * 2.55).toString(16));
+      p5.card.image(_img, 0, 0, _width, _height);
+    } catch (e) {
+      console.log(e);
+      p5.card.image(errorImage, 0, 0, _width, _height);
+    }
+    p5.card.pop();
   }
 }
 
